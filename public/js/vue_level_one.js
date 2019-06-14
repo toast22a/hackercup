ae763a5e6db7f5aa9bb1c3fd3c84fc9309e9c831
Luis Enrico Lopez <luis_lopez@dlsu.edu.ph>
2019-06-15 05:45:13 +0800

    Advance to level 2 after 3 success

--- public/js/vue_level_one.js
+++ public/js/vue_level_one.js
@@ -4,13 +4,18 @@ var vueLevelOne = new Vue({
     prompt: null,
     choices: [],
     answer: null,
-    utterance: null
+    utterance: null,
+    successCount: 0
   },
   created: function() {
     this.generateQuestion()
   },
   methods: {
     generateQuestion: function() {
+      if (this.successCount >= 3) {
+        window.location.href = '/level/2'
+        return
+      }
       axios.get('/question/1')
       .then((response)=>{
         this.prompt = response.data.prompt
@@ -28,17 +33,22 @@ var vueLevelOne = new Vue({
       correctIcon.style.display = "none"
       wrongIcon.style.display = "none"
       if (this.answer == choice) {
+        this.successCount += 1
         var buttons = document.getElementsByTagName("button")
         for (var i=0; i<buttons.length; i++) {
           buttons[i].disabled = true
         }
         correctIcon.style.display = "block"
-        setTimeout(function() {
-          window.location.href = "/level/1"
+        setTimeout(()=>{
+          correctIcon.style.display = "none"
+          this.generateQuestion()
+          for (var i=0; i<buttons.length; i++) {
+            buttons[i].disabled = false
+          }
         }, 2000)
       } else {
         wrongIcon.style.display = "block"
-        setTimeout(function() {
+        setTimeout(()=>{
           wrongIcon.style.display = "none"
         }, 2000)
       }
